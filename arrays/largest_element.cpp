2041a5314fa8944cd9b8ed6164f494b9caae72d9
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2025-11-19 11:12:53 +0530

    arrays - 1

---
+++ arrays/largest_element.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int FindMaxElement(int arr[],int n) {
+    int max = INT32_MIN;
+    for(int i=0;i< n;i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        } 
+    }
+    return max;
+}
+
+int main() {
+    int arr[] =  {1,2,4,7,7,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int c = FindMaxElement(arr,n);
+    cout<<c;
+}
+
---
+++ arrays/second_largest.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int FindSecondMaxElement(int arr[],int n) {
+    if (n < 2) return -1; 
+    int max = INT32_MIN;
+    int second_max = max;
+    for(int i=0;i< n;i++) {
+        if(arr[i] > max) {
+            second_max = max;
+            max = arr[i];
+        } else if ((arr[i] > second_max) && (arr[i] < max)) {
+            second_max = arr[i];
+        }
+    }
+    return second_max;
+}
+
+int FindSecondMinElement(int arr[],int n) {
+    if (n < 2) return -1; 
+    int min = INT32_MAX;
+    int second_min = min;
+    for(int i=0;i< n;i++) {
+        if(arr[i] < min) {
+            second_min = min;
+            min = arr[i];
+        } else if ((arr[i] < second_min) && (arr[i] > min)) {
+            second_min = arr[i];
+        }
+    }
+    return second_min;
+}
+
+int main() {
+    int arr[] =  {3,1,1,1,1,2,2,2,4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int c = FindSecondMinElement(arr,n);
+    cout<<c;
+}
+
