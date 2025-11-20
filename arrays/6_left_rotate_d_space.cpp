43ab09facf9f04ec1dec3d2f0173afea6a41b160
New-PC <aamir@atlsystems.co.uk>
2025-11-20 13:41:30 +0530

    added rotate d space

--- arrays/6_left_rotate_d_space.cpp
+++ arrays/6_left_rotate_d_space.cpp
@@ -1,13 +1,28 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include <algorithm>
 using namespace std;
 
-int rotateLeftArray(int arr[],int n,int d) {
-    int temp = arr[0];
-    for(int i=d;i<n;i++) { 
-        arr[i-1] = arr[i];
-    }
-    arr[n-1] = temp;
-    
+// int rotateLeftArray(int arr[],int n,int d) { 
+//     d = d % 2;
+//     int temp[d];
+//     for (int i = 0; i < d; i++) {
+//         temp[i] = arr[i];
+//     }
+// n-d => 4
+//     for(int i=d;i<n;i++) { 
+//         arr[i-d] = arr[i];
+//     }   
+     
+//     for(int i=n-d;i<n;i++) {
+//         arr[i] = temp[i-(n-d)]; 
+//     }
+// }
+
+// time complexity d + (n-d) + n => d + n - d + n => 2n ===> O(2n) | O(n)
+int rotateLeftArray(int arr[],int n,int d) { 
+    reverse(arr,arr+d); // O(d)
+    reverse(arr+d,arr+n); // O(n - d)
+    reverse(arr,arr+n); // O(n)
 }
 
 void printArray(int arr[], int n) {
@@ -20,8 +35,11 @@ void printArray(int arr[], int n) {
 
 int main() {
     int arr[] = {1,2,3,4,5,6,7};
+    // {4,5,6,7,1,2,3}
+    // 1,2,3 -> 3,2,1 || 4,5,6,7 -> 7,6,5,4 => {3,2,1,7,6,5,4}
+    // {4,5,6,7}
     int n = sizeof(arr) / sizeof(arr[0]);
-    rotateLeftArray(arr,n,2);
+    rotateLeftArray(arr,n,3);
     printArray(arr, n);
     return 0;
 }
