944628c95a0cbe8c45371343604fd5d75aafa077
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2025-12-03 01:32:10 +0530

    longest consecutive sequence updated

--- arrays/23_longest_consecutive_seq.cpp
+++ arrays/23_longest_consecutive_seq.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
+
+// there is one more approach of using sort there tc is o(n log n)
 void LongestConsecutiveSeq(vector<int> v1) {
-    sort(v1.begin(),v1.end());
+    unordered_set<int> uniqueElm;
     int longestSeq = 0;
-    int ctr = 0;
-    int lastMin = INT_MIN;
     for(int i=0;i<v1.size();i++) {
-        if(v1[i] - 1 == lastMin) {
-            ctr++;
-            lastMin = v1[i];
-        } else if (v1[i] - 1 != lastMin) {
-            ctr = 1;
-            lastMin = v1[i];
-        }
-        longestSeq = max(ctr,longestSeq);
+        uniqueElm.insert(v1[i]);
     } 
+    for(auto elm:uniqueElm) {
+        if(uniqueElm.find(elm - 1) == uniqueElm.end()) {
+            int ctr = 1; 
+            int x = elm;
+            while(uniqueElm.find(x+1) != uniqueElm.end()) {
+                x++;
+                ctr++;
+            }
+            longestSeq = max(longestSeq,ctr);
+        }
+    }
     cout<<"Longest Consecutive Seqence length: "<<longestSeq;
 }
 
